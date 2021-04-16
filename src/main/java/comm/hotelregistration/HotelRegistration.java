700022faa81f4eafa208e6fa5865964cd6853a04
ganesh-48 <ganeshsrananavare@gmail.com>
2021-04-16 09:00:58 -0700

    ADDED: welcome to hotel registration

---
+++ src/main/java/comm/hotelregistration/HotelRegistration.java
@@ -0,0 +1,7 @@
+package comm.hotelregistration;
+
+public class HotelRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to hotel registration");
+    }
+}
---
+++ src/test/java/com/hotelregistration/HotelRegistrationTest.java
@@ -0,0 +1,4 @@
+package com.hotelregistration;
+
+public class HotelRegistrationTest {
+}
